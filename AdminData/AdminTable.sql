c59658b94da1c7e8b61a60c33a5f34b45ade3816
JamieTalbot <jamietalbot@exe-coll.ac.uk>
2022-08-30 17:40:32 +0100

    Add project files.

---
+++ AdminData/AdminTable.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Admins] (
+    [UserID]   INT           IDENTITY (1, 1) NOT NULL,
+    [UserName] VARCHAR (50)  NOT NULL,
+    [Password] VARCHAR (100) NOT NULL,
+    PRIMARY KEY CLUSTERED ([UserID] ASC)
+);
